09728510e915b862e5b16fef472f059226781469
Andrea Testino <aitestino@gmail.com>
2026-01-20 16:02:52 -0500

    chore(nac_test): add SPDX license headers to root module

--- nac_test/pyats_core/broker/connection_broker.py
+++ nac_test/pyats_core/broker/connection_broker.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (c) 2025 Daniel Schmidt
+
 """Connection broker service for managing persistent device connections.
 
 This service runs as a long-lived daemon process that:
