2cfc9874a788299e1a9dd9612d0e20ba764ffccd
Oliver Boehmer <oboehmer@cisco.com>
2026-02-02 16:24:13 +0100

    add license header

--- tests/unit/robot/test_orchestrator.py
+++ tests/unit/robot/test_orchestrator.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MPL-2.0
+# Copyright (c) 2025 Daniel Schmidt
+
 # mypy: disable-error-code="no-untyped-def,method-assign"
 # SPDX-License-Identifier: MPL-2.0
 # Copyright (c) 2025 Daniel Schmidt
